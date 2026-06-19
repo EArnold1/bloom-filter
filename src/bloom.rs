8a55d0f245de26f2957986c5ef96cba8246a6fe7
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2026-06-19 14:12:17 +0100

    Re-implement bloom filter to derive the drivers for the algorithm

--- src/bloom.rs
+++ src/bloom.rs
@@ -2,39 +2,94 @@ use simplehash::murmurhash3_128;
 
 // Create traits for the `Filter`
 
+// m = filter size, k = no. of hash functions, p = false positive probability, n = no. of items(expected capacity)
 pub struct Filter {
     list: Vec<bool>,
-    size: usize,     // size of the list
-    hash_count: u32, // number of hash functions
+    list_size: usize,       // Expected number of items to be stored in the filter
+    pub hash_count: usize,  // The optimal number of hash functions
+    pub filter_size: usize, // Bloom filter size
 }
 
-const DEFAULT_LIST_SIZE: usize = 16; // default size
-const DEFAULT_HASH_COUNT: u32 = 3; // default size
+const DEFAULT_LIST_SIZE: usize = 30; // default size
+
+pub struct FilterBuilder {
+    false_positive_probability: f64, // [error rate] between 0 and 1
+    list_size: usize,                // expected number of items to be stored in the filter
+}
+
+impl Default for FilterBuilder {
+    fn default() -> Self {
+        FilterBuilder {
+            false_positive_probability: 0.05,
+            list_size: DEFAULT_LIST_SIZE,
+        }
+    }
+}
+
+impl FilterBuilder {
+    pub fn new(list_size: usize, false_positive_probability: f64) -> Self {
+        assert!((0_f64..=1_f64).contains(&false_positive_probability));
+
+        Self {
+            false_positive_probability,
+            list_size,
+        }
+    }
+}
+
+impl FilterBuilder {
+    /// Size of the bloom filter in bits
+    fn get_size(&self) -> usize {
+        // m = -(n * math.log(p))/(math.log(2)**2)
+        // where n = self.size, p = self.false_positive_probability
+        let list_size: f64 = self.list_size as f64;
+        let filter_size = -(list_size * self.false_positive_probability.ln()) / 2_f64.ln().powi(2);
+
+        filter_size.round() as usize
+    }
+
+    fn get_hash_count(&self, filter_size: usize) -> usize {
+        // k = (m/n) * lg(2)
+        // where m = size of filter(self.filter_size), n = list_size
+        let hash_count = (filter_size / self.list_size) as f64 * 2_f64.ln();
+
+        hash_count.round() as usize
+    }
+
+    pub fn build(self) -> Filter {
+        let filter_size = self.get_size();
+        let hash_count = self.get_hash_count(filter_size);
+        Filter {
+            list: vec![false; self.list_size],
+            list_size: self.list_size,
+            hash_count,
+            filter_size,
+        }
+    }
+}
 
 impl Default for Filter {
     fn default() -> Self {
-        let list = vec![false; DEFAULT_LIST_SIZE];
-        println!("size {}", list.len());
-        Filter {
-            list,
-            size: DEFAULT_LIST_SIZE,
-            hash_count: DEFAULT_HASH_COUNT,
-        }
+        FilterBuilder::default().build()
     }
 }
 
 impl Filter {
     // TODO: Accept bytes
     pub fn insert(&mut self, item: &str) {
-        for i in 0..self.hash_count {
-            let digest = (murmurhash3_128(item.as_bytes(), i) as usize) % self.size;
+        let hash_count = self.hash_count;
+        for i in 0..hash_count {
+            let seed = u32::try_from(i).expect("[Insert]: seed should be within u32 range");
+            let digest = (murmurhash3_128(item.as_bytes(), seed) as usize) % self.list_size;
             self.list[digest] = true
         }
     }
 
     pub fn check(&self, item: &str) -> bool {
         for i in 0..self.hash_count {
-            let digest = (murmurhash3_128(item.as_bytes(), i) as usize) % self.size;
+            let seed = u32::try_from(i).expect("[Check]: seed should be within u32 range");
+
+            let digest = (murmurhash3_128(item.as_bytes(), seed) as usize) % self.list_size;
 
             if !self.list[digest] {
                 return false;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use crate::bloom::Filter;
+use crate::bloom::FilterBuilder;
+use rand::seq::SliceRandom; // Provides the shuffle method
+use rand::thread_rng; // Provides the random number generator
 
 pub mod bloom;
 
 fn main() {
-    let mut filter = Filter::default();
+    let mut filter = FilterBuilder::new(20, 0.05).build();
 
-    let word_present = vec![
+    println!(
+        "filter size :{}, hash count :{}",
+        &filter.filter_size, &filter.hash_count
+    );
+
+    let mut rng = thread_rng();
+
+    let mut word_present = vec![
         "abound",
         "abounds",
         "abundance",
@@ -29,7 +38,7 @@ fn main() {
         "genial",
     ];
 
-    let word_absent = vec![
+    let mut word_absent = vec![
         "bluff",
         "cheater",
         "hate",
@@ -48,7 +57,22 @@ fn main() {
         filter.insert(item);
     }
 
-    let test_words = [word_present.clone(), word_absent.clone()].concat();
+    // word_present.shuffle(&mut rng);
+    // word_absent.shuffle(&mut rng);
+
+    let mut test_words = [
+        word_present.iter().take(10).cloned().collect(),
+        word_absent.clone(),
+    ]
+    .concat();
+
+    // test_words.shuffle(&mut rng);
+
+    // if filter.check("Arnold") {
+    //     println!("Probably Present")
+    // } else {
+    //     println!("definitely absent")
+    // }
 
     for word in test_words {
         if filter.check(word) {
